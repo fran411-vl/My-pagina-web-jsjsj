b9d780ae1ae82dd245ef0440cbd31b712028339e
BerryMaddy <mtfigueroapino@gmail.com>
2026-08-16 19:48:43 -0400

    commit

--- contacto.php
+++ contacto.php
@@ -6,6 +6,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>        
+    margin:0;
+    padding:0;
+    background-color:#34096C;
     </head>
     <body>
         <!-- Navbar -->
